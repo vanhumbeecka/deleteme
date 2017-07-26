9d98fd7e845096312a91da675ba0802105d17dae
andriesvh <andries.vanhumbeeck@theledger.be>
2017-07-26 10:46:14 +0200

    circleci

--- main.go
+++ main.go
@@ -4,14 +4,27 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	sc "github.com/hyperledger/fabric/protos/peer"
 )
+
+type SampleChaincode struct {
+}
+
+func (s *SampleChaincode) Init(stub shim.ChaincodeStubInterface) sc.Response {
+	return shim.Success(nil)
+}
+
+func (s *SampleChaincode) Invoke(stub shim.ChaincodeStubInterface) sc.Response {
+	return shim.Success(nil)
+}
 
 // CityHandler The CityHandler
 func CityHandler(res http.ResponseWriter, req *http.Request) {
 	data, _ := json.Marshal("{'cities': 'San Franciso, Amsterdam, Berlin'}")
 	res.Header().Set("Content-Type", "application/json; charset=utf-8")
 	res.Write(data)
-
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
 func TestHandleIndexReturnsWithStatusOK(t *testing.T) {
@@ -14,5 +16,12 @@ func TestHandleIndexReturnsWithStatusOK(t *testing.T) {
 
 	if response.Code != http.StatusOK {
 		t.Fatalf("Non-expected status code%v:\n\tbody: %v", "200", response.Code)
+	}
+}
+
+func TestCreateLoanApplication(t *testing.T) {
+	stub := shim.NewMockStub("mockstub", new(SampleChaincode))
+	if stub == nil {
+		t.Fatalf("MockStub creation failed")
 	}
 }
